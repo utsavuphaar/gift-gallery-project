1e1aefbd345ce9ac316eaac92e3ef99c50d1fd9a
Rahul <rahulvishwakarma6737@gmail.com>
2024-04-06 11:54:33 +0530

    my first

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import Product from './Components/User/Product';
+import Product from './Components/User/Product.js';
 import { Route, Routes } from 'react-router-dom';
-import ViewMore from './Components/User/ViewMore';
+import ViewMore from './Components/User/ViewMore.js';
+import SignUp from './Components/User/Signup.js';
 
 function App() {
-  return<>
+  return <>
     <Routes>
-      <Route  path="/" element={<Product/>}/>
-      <Route path='/viewmore' element={<ViewMore/>}/>
+      <Route path='/signup' element={<SignUp />} />
+      <Route path="/" element={<Product />} />
+      <Route path='/viewmore' element={<ViewMore />} />
     </Routes>
   </>
 }
--- src/Components/User/Signup.js
+++ src/Components/User/Signup.js
@@ -1,5 +1,18 @@
-export default function Signup(){
+import '../../CSS/Signup.css'
+
+export default function SignUp(){
     return <>
-      <h1>Sign up component...</h1>
+        <div className='container-fluid d-flex justify-content-center align-items-center border main'>
+            <div className='row border signup-page'>
+                <div className='col-md-6 border image'>
+
+                </div>
+                <div className='col-md-6 border left'>
+                    <h2 className='mt-4 ms-4'>Create an account</h2>
+                    <span className='ms-4'>Enter your details below</span>
+                    {/* <TextField id="standard-basic" label="Standard" variant="standard" /> */}
+                </div>
+            </div>
+        </div>
     </>
 }
