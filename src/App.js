8eb37895936a1a5e84cc14c702eea7487770c510
Rahul <rahulvishwakarma6737@gmail.com>
2024-04-06 15:19:37 +0530

    sign up form

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Product from './Components/User/Product';
 import { Route, Routes } from 'react-router-dom';
 import ViewMore from './Components/User/ViewMore';
 import CartItems from './Components/User/CartItems';
+import Signup from './Components/User/Signup.js';
 
 function App() {
   return<>
     <Routes>
       <Route  path="/" element={<Product/>}/>
+      <Route path='/signup' element={<Signup/>}/>
       <Route path='/viewmore' element={<ViewMore/>}/>
       <Route  path="/cartitems" element={<CartItems/>}/>
     </Routes>
