1cece69d13e9a4659e829be10617224f240e97e6
Rahul <rahulvishwakarma6737@gmail.com>
2024-04-19 13:00:49 +0530

    Update Product.js

--- src/Components/User/Product.js
+++ src/Components/User/Product.js
@@ -1,3 +1,5 @@
+// ---------------Filters-------------------
+
 import * as React from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
@@ -6,7 +8,16 @@ import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Fade from '@mui/material/Fade';
 
+// -------------------Price filter---------------------------
+
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+
+// -------------------------------
+
 import { ImStarFull } from "react-icons/im";
+
+// --------------------Productlist-----------------
 
 import { useDispatch, useSelector } from "react-redux"
 import store from "../../Store/store"
@@ -15,6 +26,15 @@ import { CiHeart } from "react-icons/ci";
 import { useEffect } from "react";
 import { addProductIntoCart, addProductIntoWishlist, fetchProduct } from "../../DataSlice/ProductSlice";
 import { Link, useNavigate } from "react-router-dom";
+
+// -------------price range----------------
+
+function valuetext(value) {
+    return `${value}°C`;
+}
+
+// -----------------------------------
+
 export default function Product() {
     let userId = localStorage.getItem("userId")
     const dispatch = useDispatch();
@@ -41,12 +61,18 @@ export default function Product() {
     };
 
 
+    const [value, setValue] = React.useState([20, 37]);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     return <>
-        <div className="container-fluid border">
-            <div className="row p-0 border ">
+        <div className="container-fluid " style={{backgroundColor:"#F7FAFC"}}>
+            <div className="row p-0 border mb-3 ">
                 <div className="col-lg-3 p-0">
                     <div>
-                        <Accordion
+                        <Accordion style={{backgroundColor:"#F7FAFC",boxShadow:"none",border:"none"}}
                             expanded={expanded}
                             onChange={handleExpansion}
                             slots={{ transition: Fade }}
@@ -61,7 +87,7 @@ export default function Product() {
                                 aria-controls="panel1-content"
                                 id="panel1-header"
                             >
-                                <Typography className='text-primary'>Brand</Typography>
+                                <Typography className='text-dark fw-bold'>Brand</Typography>
                             </AccordionSummary>
                             <AccordionDetails>
                                 <Typography>
@@ -74,13 +100,13 @@ export default function Product() {
                                 </Typography>
                             </AccordionDetails>
                         </Accordion>
-                        <Accordion>
+                        <Accordion style={{backgroundColor:"#F7FAFC",boxShadow:"none",border:"none"}}>
                             <AccordionSummary
                                 expandIcon={<ExpandMoreIcon />}
                                 aria-controls="panel2-content"
                                 id="panel2-header"
                             >
-                                <Typography className="text-primary">Feature</Typography>
+                                <Typography className="text-dark fw-bold">Feature</Typography>
                             </AccordionSummary>
                             <AccordionDetails>
                                 <Typography>
@@ -94,60 +120,91 @@ export default function Product() {
                                 <span>Rahul Vishwakarma</span>
                             </AccordionDetails>
                         </Accordion>
-                        <Accordion>
+                        <Accordion style={{backgroundColor:"#F7FAFC",boxShadow:"none",border:"none"}}>
                             <AccordionSummary
                                 expandIcon={<ExpandMoreIcon />}
                                 aria-controls="panel2-content"
                                 id="panel2-header"
                             >
-                                <Typography className="text-primary">Rating</Typography>
+                                <Typography className="text-dark fw-bold">Price range</Typography>
                             </AccordionSummary>
                             <AccordionDetails>
-                                {/* <Typography>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                                    malesuada lacus ex, sit amet blandit leo lobortis eget.
-                                </Typography> */}
-                                
-                               
+                                <Box sx={{ width: 300 }}>
+                                    <Slider
+                                        getAriaLabel={() => 'Temperature range'}
+                                        value={value}
+                                        onChange={handleChange}
+                                        valueLabelDisplay="auto"
+                                        getAriaValueText={valuetext}
+                                    />
+                                </Box>
+                                <div className='container d-flex flex-column justify-content-center align-items-center'>
+                                    <div className='row d-flex'>
+                                        <div className='col-sm-6 d-flex flex-column justify-content-center align-items-center'>
+                                            <label>Min</label>
+                                            <input className='w-50' style={{height:"30px",border:'1px solid blue',outline:'none',borderRadius:"5px"}}  type='number' min={0} />
+                                        </div>
+                                        <div className='col-sm-6 d-flex flex-column align-items-center'>
+                                            <label>Max</label>
+                                            <input className='w-50' style={{height:"30px",paddingLeft:"5px",border:'1px solid blue',outline:'none',borderRadius:"5px"}} type='number' min={9999} />
+                                        </div>
+                                    </div>
+                                    <button className='w-75 mt-3 btn btn-outline-info'>Apply</button>
+                                </div>
+
+                            </AccordionDetails>
+                        </Accordion>
+                        <Accordion style={{backgroundColor:"#F7FAFC",boxShadow:"none",border:"none"}}>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel2-content"
+                                id="panel2-header"
+                            >
+                                <Typography className="text-dark fw-bold">Rating</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+
+
+
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                        <label class="form-check-label" for="flexCheckDefault">
-                                        <ImStarFull className='text-warning'/>
-                                        <ImStarFull className='text-warning'/>
-                                        <ImStarFull className='text-warning'/>
-                                        <ImStarFull className='text-warning'/>
-                                        <ImStarFull className='text-warning'/>
-                                        </label>
+                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label class="form-check-label" for="flexCheckDefault">
+                                        <ImStarFull className='text-warning' />
+                                        <ImStarFull className='text-warning' />
+                                        <ImStarFull className='text-warning' />
+                                        <ImStarFull className='text-warning' />
+                                        <ImStarFull className='text-warning' />
+                                    </label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                        <label class="form-check-label" for="flexCheckDefault">
-                                        <ImStarFull className='text-warning'/>
-                                        <ImStarFull className='text-warning'/>
-                                        <ImStarFull className='text-warning'/>
-                                        <ImStarFull className='text-warning'/>
-                                        <ImStarFull className='text-secondary'/>
-                                        </label>
+                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label class="form-check-label" for="flexCheckDefault">
+                                        <ImStarFull className='text-warning' />
+                                        <ImStarFull className='text-warning' />
+                                        <ImStarFull className='text-warning' />
+                                        <ImStarFull className='text-warning' />
+                                        <ImStarFull className='text-secondary' />
+                                    </label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                        <label class="form-check-label" for="flexCheckDefault">
-                                        <ImStarFull className='text-warning'/>
-                                        <ImStarFull className='text-warning'/>
-                                        <ImStarFull className='text-warning'/>
-                                        <ImStarFull className='text-secondary'/>
-                                        <ImStarFull className='text-secondary'/>
-                                        </label>
+                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label class="form-check-label" for="flexCheckDefault">
+                                        <ImStarFull className='text-warning' />
+                                        <ImStarFull className='text-warning' />
+                                        <ImStarFull className='text-warning' />
+                                        <ImStarFull className='text-secondary' />
+                                        <ImStarFull className='text-secondary' />
+                                    </label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                        <label class="form-check-label" for="flexCheckDefault">
-                                        <ImStarFull className='text-warning'/>
-                                        <ImStarFull className='text-warning'/>
-                                        <ImStarFull className='text-secondary'/>
-                                        <ImStarFull className='text-secondary'/>
-                                        <ImStarFull className='text-secondary'/>
-                                        </label>
+                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label class="form-check-label" for="flexCheckDefault">
+                                        <ImStarFull className='text-warning' />
+                                        <ImStarFull className='text-warning' />
+                                        <ImStarFull className='text-secondary' />
+                                        <ImStarFull className='text-secondary' />
+                                        <ImStarFull className='text-secondary' />
+                                    </label>
                                 </div>
                             </AccordionDetails>
                         </Accordion>
@@ -155,7 +212,7 @@ export default function Product() {
                 </div>
                 <div className="col-lg-9 p-0 border d-flex flex-wrap justify-content-around align-items-center">
                     {productList?.map((product, index) => <div className=" mt-2 col-lg-4 d-flex justify-content-center align-items-center">
-                        <div style={{ width: "280px" }} className="border p-2 m-2 gift-card">
+                        <div style={{ width: "280px" }} className="bg-white border p-2 m-2 gift-card">
 
                             <img src={product.thumbnail} onClick={() => viewMore(product)} style={{ cursor: 'pointer', width: "250px", height: "230px", borderRadius: "10px" }} />
                             <div className="w-100">
