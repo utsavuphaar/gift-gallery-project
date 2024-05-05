ba4782251d6f143612bd0c25dc89fa894aac7ed2
Utsavuphaar <infoutsavuphaar@gmail.com>
2024-05-05 21:21:58 +0530

    order get

--- src/Components/DeliveryBoy/GetOrders.js
+++ src/Components/DeliveryBoy/GetOrders.js
@@ -5,7 +5,9 @@ import ApiUrl from '../ApiUrl';
 import { useNavigate } from 'react-router-dom';
 function GetOrders() {
     const navigate = useNavigate();
-    let userId = localStorage.getItem("userId");
+    const [, forceUpdate] = useReducer(x => x + 1, 0);
+    var deliveryBoy = localStorage.getItem("deliveryBoy");
+    deliveryBoy = JSON.parse(deliveryBoy);
     const [state, dispatch] = useReducer((state, action) => {
         if (action.type === "set-order") {
             return { ...state, orderList: action.payload };
@@ -29,39 +31,53 @@ function GetOrders() {
             })
     }, []);
 
+    let orderList = state.orderList.filter(order => order.status === 'Order Confirmed');
+    const getProduct = async (orderItemId, userId,orderId) => {
+        // Assuming deliveryBoy is defined somewhere accessible in your code
+        // alert(orderItemId + " " + userId + " " + deliveryBoy.id+" "+orderId);
+     const result =   await axios.post(ApiUrl.getOrder,{deliveryBoyId:deliveryBoy.id,orderItemId:orderItemId,userId:userId})
+        if(result){
+            await axios.put(ApiUrl.updateOrderStatus,{id:orderId,status:"Out for delivery"});
+            alert("Order get Successfully...")
+            forceUpdate();
+        }
+    };
+    
+
     return (
         <>
             <div className='container-fluid w-75 p-2' style={{ backgroundColor: "#f7fafc" }}>
                 <div className='container p-2 mt-4 row ' style={{ overflow: "auto", height: '550px' }}>
                     <table className='table border  col-md-10 position-relative' style={{ maxHeight: '80vh', overflow: "scroll" }}>
-                        <thead className='position-sticky' style={{top:'-10px'}} >
+                        <thead className='position-sticky' style={{ top: '-10px' }} >
                             <tr className='bg-primary text-center text-white'>
 
                                 <th>Sr.No.</th>
                                 <th>Item</th>
                                 <th>Name</th>
                                 <th>Price</th>
-                
                                 <th>Order Date</th>
                                 <th>Order Id</th>
                             </tr>
                         </thead>
                         <tbody className='bg-light'>
-                            {state.orderList?.map((order, index) => <tr key={index} className='text-center'>
+                            {orderList?.map((order, index) => <tr key={index} className='text-center'>
                                 <td>{index + 1}</td>
-                                
+
                                 {order?.orderItems.map((data, ind) => <td key={ind}>
-                                    <img src={data.product.thumbnail} onClick={()=>navigate("/orderDetail",{state:order})} style={{cursor:'pointer'}} width="100px" height="100px" />
+                                    <img src={data.product.thumbnail} onClick={() => navigate("/orderDetail", { state: order })} style={{ cursor: 'pointer' }} width="100px" height="100px" />
                                 </td>)}
                                 {order?.orderItems.map((data, ind) => <td key={ind}>
-                                    {data.product.title}
+                                    {data.product.title.slice(0, 30)}
                                 </td>)}
                                 {order?.orderItems.map((data, ind) => <td key={ind}>
                                     {data.product.price}
                                 </td>)}
-                              
+
                                 <td>{order.orderDate}</td>
-                                <td className='text-secondary' style={{ cursor: "pointer" }}><button className='btn btn-outline-primary'>Get Order</button></td>
+                                {order?.orderItems.map((data, ind) => <td key={ind}>
+                                    <button onClick={() => getProduct(data.id, order.userId,order.id)} className='btn btn-outline-primary'>Get Order</button>
+                                </td>)}
                             </tr>)}
                         </tbody>
                     </table>
